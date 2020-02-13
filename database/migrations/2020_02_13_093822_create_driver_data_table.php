337d61832d6d8779da5ee8610e278a05a7902daa
mutebi nuhu <mutebinuhu1@gmail.com>
2020-02-13 06:55:04 +0000

    creating the driver form

---
+++ database/migrations/2020_02_13_093822_create_driver_data_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriverDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driver_data', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('reg_no', 30);
+            $table->string('make', 30);
+            $table->string('eng_no', 100);
+            $table->string('chasis_no', 100);
+            $table->date('date', 40);
+            $table->string('mileage', 50);
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver_data');
+    }
+}
